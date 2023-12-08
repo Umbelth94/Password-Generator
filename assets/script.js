058b4f5983a65e233f67667b15f8e667073cb73a
Travis Umbel <Umbelth94@gmail.com>
2023-12-08 16:13:55 -0600

    added array combiner, randomizer, and shuffler.  It is basically fully functional

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,16 @@
 // Assignment code here
 function generatePassword(){
-  var lowerCaseLet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  var numbers = '0123456789'.split('');
-  var symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
-  //Create Array for lowercase letters
-  //Create Array for uppercase letters
-  //Create Array for numbers
-  //Create array for symbols
+  var lowercaseLets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var uppercaseLets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  var numChars = '0123456789'.split('');
+  var symbolsChars = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
 
   var password = '';
-  //Create a password variable that is an empty string
+  var combinedArrays =[];
 
   var passwordLength = Number(prompt('How many characters would you like in your password?'));
-  if (typeof(passwordLength != 'number')){
+  console.log(typeof passwordLength);
+  if (typeof passwordLength !== 'number'){
     alert('You must type a number');
     //Maybe a better way to repeat?
     passwordLength = Number(prompt('How many characters would you like in your password?'));
@@ -22,26 +19,79 @@ function generatePassword(){
     alert('Your password must be at least 8 and no more than 128 characters');
     passwordLength = Number(prompt('How many characters would you like in your password?'));
   };
-  //Prompt user for password length and store in a variable
 
   var addLowercase = confirm('Do you want to include lowercase letters?');
-  var addUppercase = confirm('Do you want to include lowercase letters?');
+  console.log(addLowercase);
+  var addUppercase = confirm('Do you want to include uppercase letters?');
+  console.log(addUppercase);
   var addNumbers = confirm('Do you want to include numbers?');
-  var symbols = confirm('Do you want to include symbols?');
-  //Prompt user for if they want lowercase and store in a boolean
-  //Prompt user for if they want uppercase and store in a boolean
-  //Prompt user for if they want numbers and store in a boolean
-  //Prompt user for if they want symbols and store in a boolean
+  console.log(addNumbers);
+  var addSymbols = confirm('Do you want to include symbols?');
+  console.log(addSymbols);
 
-  //Add together any arrays that they wanted to include based on the true/false values of the booleans into it's own array
-    //If lowercase is true then add lowercase array to combined array
-    //If uppercase is true true then add lowercase array to combined array
-    //if numbers is true then add lowercase array to combined array
-    //if symbols is true then add lowercase array to combined array
+//Add the selected arrays to a combined pool of arrays
+if (addLowercase){
+  combinedArrays = combinedArrays.concat(lowercaseLets);
+  passwordLength -= 1; //Subtract password length by 1 because we're adding an element to the password variable before the loop starts
+  console.log('New password length: ' + passwordLength);
+  pullRandomIndex(lowercaseLets); //Pull one index element out to guarantee that this array gets used in the password
+}
+if (addUppercase){
+  combinedArrays =  combinedArrays.concat(uppercaseLets);
+  passwordLength -= 1; 
+  console.log('New password length: ' + passwordLength);
+  pullRandomIndex(uppercaseLets); 
+}
+if (addNumbers){
+  combinedArrays =  combinedArrays.concat(numChars);
+  passwordLength -= 1; 
+  console.log('New password length: ' + passwordLength);
+  pullRandomIndex(numChars); 
+}
+if (addSymbols){
+  combinedArrays = combinedArrays.concat(symbolsChars);
+  passwordLength -= 1; 
+  console.log('New password length: ' + passwordLength);
+  pullRandomIndex(symbolsChars); 
+}
 
-  //Randomize Array and pull the password length amount of elements out of it and put it into the password variable.
-  
+function pullRandomIndex(array){
+  var randomIndex = Math.floor(Math.random() * array.length);
+  password += array[randomIndex];
+  console.log('Added to password: ' + array[randomIndex]);
+  console.log('password is now ' + password);
+  return password;
+}
 
+//Loop through the combined Arrays with a random index and add that value to the password.
+for (i=0; i < passwordLength ; i++){
+  pullRandomIndex(combinedArrays);
+  // console.log(passwordLength)
+  // var randomIndex = Math.floor(Math.random() * combinedArrays.length);
+  // console.log('randomIndex is ' + randomIndex);
+  // console.log('That value is ' + combinedArrays[randomIndex]);
+  // password += combinedArrays[randomIndex];
+  // console.log('Added to password.  Password is now: ' + password);
+}
+
+//shuffle the password so that the guaranteed values aren't always at the beginning of the password string
+String.prototype.shuffle = function () {
+  console.log('shuffling password');
+  var a = this.split(""),
+      n = a.length;
+
+  for(var i = n - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tmp = a[i];
+      a[i] = a[j];
+      a[j] = tmp;
+  }
+  return a.join("");
+}
+console.log('original password ' + password);
+password = password.shuffle();
+console.log('Shuffled password is now ' + password);
+return password;
 }
 
 
